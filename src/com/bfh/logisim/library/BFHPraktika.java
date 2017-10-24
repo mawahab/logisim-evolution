45ea767a211baf175f20377de94e490639b97318
sderrien <sderrien@irisa.fr>
2017-10-24 17:10:03 +0200

    Added BinTo7seg component with support for VHDL generation

--- src/com/bfh/logisim/library/BFHPraktika.java
+++ src/com/bfh/logisim/library/BFHPraktika.java
@@ -9,37 +9,40 @@ import com.cburch.logisim.tools.Tool;
 
 public class BFHPraktika extends Library {
 	private static FactoryDescription[] DESCRIPTIONS = {
-		new FactoryDescription("Binairy_to_BCD_converter",
-		         Strings.getter("Bin2BCD"), "",
-		         "bin2bcd"), 
-		new FactoryDescription("BCD_to_7_Segment_decoder",
-		         Strings.getter("BCD2SevenSegment"), "",
-		         "bcd2sevenseg"),};
+			new FactoryDescription("Bin_to_7_Segment_decoder",
+					Strings.getter("Bin2SevenSegment"), "",
+					"bin2sevenseg"), 
+			new FactoryDescription("Binary_to_BCD_converter",
+					Strings.getter("Bin2BCD"), "",
+					"bin2bcd"), 
+			new FactoryDescription("BCD_to_7_Segment_decoder",
+					Strings.getter("BCD2SevenSegment"), "",
+					"bcd2sevenseg"),};
 
-private List<Tool> tools = null;
+	private List<Tool> tools = null;
 
-public BFHPraktika() {
-}
-
-@Override
-public String getDisplayName() {
-	return Strings.get("BFH mega functions");
-}
-
-@Override
-public String getName() {
-	return "BFH-Praktika";
-}
-
-@Override
-public List<Tool> getTools() {
-	if (tools == null) {
-		tools = FactoryDescription.getTools(BFHPraktika.class, DESCRIPTIONS);
+	public BFHPraktika() {
 	}
-	return tools;
-}
 
-public boolean removeLibrary(String Name) {
-	return false;
-}
+	@Override
+	public String getDisplayName() {
+		return Strings.get("BFH mega functions");
+	}
+
+	@Override
+	public String getName() {
+		return "BFH-Praktika";
+	}
+
+	@Override
+	public List<Tool> getTools() {
+		if (tools == null) {
+			tools = FactoryDescription.getTools(BFHPraktika.class, DESCRIPTIONS);
+		}
+		return tools;
+	}
+
+	public boolean removeLibrary(String Name) {
+		return false;
+	}
 }
